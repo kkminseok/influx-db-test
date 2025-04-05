55ad9f36214dc34d09308fa4c922fce9048c115a
kms <aaaa727@khu.ac.kr>
2025-04-05 18:26:56 +0900

    feat: grpc 샘플최신화

---
+++ grpc-server/src/main/java/com/my/grpcserver/TickerService.java
@@ -0,0 +1,34 @@
+package com.my.grpcserver;
+
+import io.grpc.stub.StreamObserver;
+import org.springframework.stereotype.Service;
+import org.toybrix.donny.TickerDetailProto;
+import org.toybrix.donny.TickerRtProto;
+import org.toybrix.donny.TickerServiceGrpc;
+import org.toybrix.donny.TickerServiceProto;
+
+@Service
+public class TickerService extends TickerServiceGrpc.TickerServiceImplBase {
+
+
+    @Override
+    public void WatchRealtime() {
+
+    }
+
+    @Override
+    public void watchRealtime(TickerServiceProto.TickerWatchRequest request, StreamObserver<TickerRtProto.TickerRt> responseObserver) {
+        // 여기에 원하는 gRPC 처리 로직을 작성
+        TickerRtProto.TickerRt response = TickerRtProto.TickerRt.newBuilder()
+                .setSymbol(request.getSymbol())
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getDetailPrice(TickerServiceProto.GetPriceRequest request, StreamObserver<TickerDetailProto.TickerDetail> responseObserver) {
+        super.getDetailPrice(request, responseObserver);
+    }
+}
